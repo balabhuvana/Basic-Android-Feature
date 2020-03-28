71e0c5e5c43aa882f269168fd1d40a24eafa2079
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-03-28 20:54:00 +0530

    UI for creation

--- app/src/main/java/com/example/basic_android_feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/basic_android_feature/home/HomeActivity.kt
@@ -9,5 +9,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        supportFragmentManager.beginTransaction().replace(R.id.myHomeContainer, HomeFragment())
+            .commit()
     }
 }
