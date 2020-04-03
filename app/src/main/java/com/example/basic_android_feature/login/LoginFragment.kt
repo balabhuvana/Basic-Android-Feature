e163fc36173c8148d4ec64580be98aa52b515753
Balamurugan Selvam <balamurugan.selvam@hcl.com>
2020-04-03 14:52:15 +0530

    UI Created for 1. Register 2. Login 3. Home 4. Setting

--- app/src/main/java/com/example/basic_android_feature/login/LoginFragment.kt
+++ app/src/main/java/com/example/basic_android_feature/login/LoginFragment.kt
@@ -1,24 +1,26 @@
-package com.example.basic_android_feature.home
+package com.example.basic_android_feature.login
 
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+
 import com.example.basic_android_feature.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
 
 }
